6a50c1924ad276cebe8160fa04e431ea7d5d4f59
tamerkat <mahad10taha@gmail.com>
2023-10-25 21:05:59 +0300

    salma is my love

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -11,8 +11,11 @@ void _print_rev_recursion(char *s)
 {
 	if (*s > "\0")
 	{
-		_print_rev_recursion(s + 1);
-		_putchar(*s);
-
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+	}
+	els
+	{
+	return (0);
 	}
 }
