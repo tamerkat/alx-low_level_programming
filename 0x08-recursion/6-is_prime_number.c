00fdae8d37a0e627fc48f093f5c3d05629a9d5d8
tamerkat <mahad10taha@gmail.com>
2023-10-25 19:33:53 +0300

    salma is my love

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * is_prime_number - check the code
+ *@s: pointer
+ * Return: Always 0.
+ */
+
+int is_prime_number(int n)
+{
+    if (n / n == 1)
+    {
+        return (1);
+    }
+    else
+    {
+        return (0);
+    }
+}
