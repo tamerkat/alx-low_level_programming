116824252e6ebaac885a24a01951a08975c3550e
tamerkat <mahad10taha@gmail.com>
2023-10-25 14:48:28 +0300

    salma is my love

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,20 +2,20 @@
 
 /**
  * _puts_recursion - check the code
- *
+ *@s: pointer
  * Return: Always 0.
  */
 
 
 void _puts_recursion(char *s)
 {
-    if (*s == "\0");
-    {
-        _putchar("\n");
-    }
-    else
-    {
-        _putchar(*s);
-        _puts_recursion(s + 1);
-    }
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
 }
