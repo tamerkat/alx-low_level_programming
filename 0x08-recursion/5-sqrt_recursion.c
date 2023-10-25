b1876aaa0ba348fcd4fb6ff068befc9c33f45de3
tamerkat <mahad10taha@gmail.com>
2023-10-25 19:26:58 +0300

    salma is my love

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - check the code
+ * @n: pointer
+ * @val:pointer
+ * Return: Always 0.
+*/
+
+int sqoure(int n, int val);
+
+int _sqrt_recursion(int n)
+{
+	sqoure(n, 1);
+}
+
+/**
+ * sqoure -Entry point
+ * @val: pointer
+ * @n: pointer
+ * Return: always 0
+*/
+
+int sqoure(int n, int val)
+{
+	if (val * val == n)
+	{
+	return (val);
+	}
+	else if (val * val < n)
+	{
+	return (sqoure(n, val + 1));
+	}
+}
