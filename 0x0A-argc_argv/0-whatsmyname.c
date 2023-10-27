932e0e3766ec53ac7ba9f38c4f90cd0c768b3d77
tamerkat <mahad10taha@gmail.com>
2023-10-27 11:36:51 +0200

    five

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Entriy poient
+ * @argc: int
+ * @argv: list show argc
+ * Return: always success
+ */
+
+int main(int argc, char **argv)
+{
+	if (argc == 1)
+	{
+	printf("%s\n", argv[0]);
+	}
+}
