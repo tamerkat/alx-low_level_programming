c7ae2d14f9593c676c3506c311d012d8bc982462
tamerkat <mahad10taha@gmail.com>
2023-10-27 13:11:16 +0200

    five

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -9,9 +9,9 @@
  * Return: always success
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char const *argv[])
 {
-	for (int i = 1; i < argc; i++)
+	for (int i = 0; i < argc; i++)
 	{
 	printf("%s\n", argv[i]);
 	}
