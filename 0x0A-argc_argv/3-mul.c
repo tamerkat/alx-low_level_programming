105752a9c65957a53563f7de276345902c7ceaed
tamerkat <mahad10taha@gmail.com>
2023-10-27 13:44:41 +0200

    five

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,14 +12,14 @@ int main(int argc, char *argv[])
 {
 	int sum;
 
-	if (argc != 3)
+	if (argc == 3)
 	{
-	printf("Error\n");
-	return (1);
+		sum = atoi(argv[1]) * atoi(argv[2]);
+	printf("%d\n", sum);
 	}
 	else
 	{
-	sum = atoi(argv[1]) * atoi(argv[2]);
-	printf("%d\n", sum);
+		printf("Error\n");
+	return (1);
 	}
 }
