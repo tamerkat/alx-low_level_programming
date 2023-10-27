7f7b8e627bc341a03f8ff4fe2eef0fd50851e10a
tamerkat <mahad10taha@gmail.com>
2023-10-27 12:30:41 +0200

    five

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,7 @@
 #include "main.h"
-
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
 /**
  * main - Entriy poient
  * @argc: int
@@ -9,9 +11,7 @@
 
 int main(int argc, char **argv)
 {
-	if (argc != 1)
-	{
-	return (1);
-	}
+	(void)argc;
 	printf("%s\n", argv[0]);
+	return (0);
 }
