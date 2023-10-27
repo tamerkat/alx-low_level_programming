83ecd245c385dcba62a97aa9684af73d5a3b36cb
tamerkat <mahad10taha@gmail.com>
2023-10-27 13:40:33 +0200

    five

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+/**
+ * main - Entriy poient
+ * @argc: int
+ * @argv: list show argc
+ * Return: always success
+ */
+
+int main(int argc, char *argv[])
+{
+	int sum;
+
+	if (argc != 3)
+	{
+	printf("Error\n");
+	return (1);
+	}
+
+	sum = atoi(argv[1]) * atoi(argv[2]);
+
+	printf("%i\n", sum);
+}
