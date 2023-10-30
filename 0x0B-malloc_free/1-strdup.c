b19ede5133127e3ccdbb551e95517e2780a660b2
tamerkat <mahad10taha@gmail.com>
2023-10-30 14:44:07 +0200

    Bon

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * *_strdup - Entry point for
+ * @str: pointer
+ * Return: Always succees
+*/
+
+char *_strdup(char *str)
+{
+	int i = 0;
+	int size = 0;
+	char *c;
+
+	if (str == NULL)
+	{
+	return (NULL);
+	}
+	for (; str[size] != '\0'; size++)
+	{
+	c = malloc(sizeof(*str) * size + 1);
+	if (c == NULL)
+	{
+	return (NULL);
+	}
+	else
+	{
+	for (; i < size; i++)
+	{
+	c[i] = str[i];
+	}
+	}
+	}
+	return (c);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -7,6 +7,6 @@
 
 
 char *create_array(unsigned int size, char c);
-
+char *_strdup(char *str);
 
 #endif /*MAIN_H*/
