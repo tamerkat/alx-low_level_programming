a4cd731551bd713e7f2fdeb3c8bd89d7ad5ae21f
tamerkat <mahad10taha@gmail.com>
2023-10-31 17:51:43 +0200

    malloc

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,59 @@
+#include "main.h"
+
+/**
+ * *strlen - Entry point for
+ * @s: pointer
+ * Return: Always succees
+*/
+
+char *strlen(char *s)
+{
+	int size = 0;
+
+	for (; s[size] != "\0"; size++)
+	;
+	return (size);
+}
+
+/**
+ * *str_concat - Entry point for
+ * @s1: pointer
+ * @s2: pointer
+ * Return: Always succees
+*/
+
+char *str_concat(char *s1, char *s2)
+{
+	int size1, size2;
+	int i = 0;
+	char *c;
+
+	size1 = strlen(s1);
+	size2 = strlen(s2);
+	if (s1 == NULL)
+	{
+	return ("\0");
+	}
+	if (s2 == NULL)
+	{
+	return ("\0");
+	}
+
+	c = malloc((size1 + size2) * sizeof(char) + 1);
+	if (c == NULL)
+	return (0);
+
+	for (; i < size1 + size2; i++)
+	{
+	if (i < size1)
+	{
+	c[i] = s1[i];
+	}
+	else
+	{
+	c[i] = s2[i - size1];
+	}
+	c[i] = '\0';
+	return (c);
+	}
+}
