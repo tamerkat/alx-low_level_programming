4cc67967b895de11656df9349c6da531b7bec816
tamerkat <mahad10taha@gmail.com>
2023-11-02 21:30:07 +0200

    ola

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -11,6 +11,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 char *_memest(char *s, char b, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
+int _putchar(char c);
 
 
 #endif /*MAIN_H*/
