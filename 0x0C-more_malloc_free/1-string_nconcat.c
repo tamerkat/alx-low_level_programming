c3057dd889dc936640665faf235ad6dc461d1d94
tamerkat <mahad10taha@gmail.com>
2023-11-02 16:44:54 +0200

    ola

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * string_nconcat - Entriy poient
+ * @s1: pointer
+ * @s2: pointer
+ * @n: pointer
+ * Return: always success
+ */
+
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int i, j, s1_leanght, s2_leanght;
+	char *str;
+
+	if (s1 == NULL)
+	{
+	s1 = ""
+	}
+	if (s2 == NULL)
+	{
+	s2 = ""
+	}
+
+	for (s1_leanght = 0; s1_leanght != "\0"; s1_leanght++)
+	;
+	for (s2_leanght = 0; s2_leanght != "\0"; s2_leanght++)
+	;
+
+	str = malloc(s1_leanght + n + 1);
+	if (str == NULL)
+	{
+	return (NULL);
+	}
+	for (i = 0; s1[i] != "\0"; i++)
+	{
+	str[i] = s1[i];
+	}
+	for (j = 0; j < n; j++)
+	{
+	str[j] = s2[j];
+	i++
+	}
+	str[i] = "\0"
+	return (str);
+}
+
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -6,7 +6,7 @@
 #include <stdlib.h>
 #include <limits.h>
 
-
+char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *malloc_checked(unsigned int b);
 
 
