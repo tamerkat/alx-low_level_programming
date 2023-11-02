f0e9fdaed54a0e4cb4d212d9317e33a64800918c
tamerkat <mahad10taha@gmail.com>
2023-11-02 20:49:38 +0200

    ola

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,35 +10,31 @@
 
 char *memest(char *s, char b, unsigned int n)
 {
-	char *ptr == s;
+	char *ptr = s;
 
 	while (n--)
-	{
-	s++ = b;
-	}
+		*s++ = b;
+
 	return (ptr);
 }
 
 /**
  * _calloc - Entry point
- * @nemb: pointer
+ * @nmemb: pointer
  * @size: pointer
  * Return: always success
- */
--
+*/
+
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *c;
 
 	if (nmemb == 0 || size == 0)
-	{
-	return (NULL);
-	}
+		return (NULL);
 	c = malloc(sizeof(int) * nmemb);
-	if (c == NULL)
-	{
-	return (NULL);
-	}
+	if (c == 0)
+		return (NULL);
 	memest(c, 0, sizeof(int) * nmemb);
+
 	return (c);
 }
