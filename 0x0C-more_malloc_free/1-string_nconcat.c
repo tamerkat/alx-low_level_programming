6ff2f931dd08d4d11b335d68eaa0d9e586f23f47
tamerkat <mahad10taha@gmail.com>
2023-11-02 20:35:31 +0200

    ola

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * string_nconcat - Entriy poient
+ * string_nconcat - Entry poient
  * @s1: pointer
  * @s2: pointer
  * @n: pointer
@@ -11,7 +11,7 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int i, j, s1_leangh, s2_leangh;
+	int i, j, s1_leanght, s2_leanght;
 	char *str;
 
 	if (s1 == NULL)
@@ -19,19 +19,16 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s2 == NULL)
 		s2 = "";
 
-	for (s1_leangh = 0; s1[s1_leangh] != '\0'; s1_leangh++)
-		;
-	for (s2_leangh = 0; s2[s2_leangh] != '\0'; s2_leangh++)
-		;
+	for (s1_leanght = 0; s1[s1_leanght] != '\0'; s1_leanght++)
+	;
+	for (s2_leanght = 0; s2[s2_leanght] != '\0'; s2_leanght++)
+	;
 
-	str = malloc(s1_leangh + n + 1);
+	str = malloc(s1_leanght + n + 1);
 	if (str == NULL)
 		return (NULL);
-
 	for (i = 0; s1[i] != '\0'; i++)
-	{
-	str[i] = s1[i];
-	}
+		str[i] = s1[i];
 	for (j = 0; j < n; j++)
 	{
 	str[i] = s2[j];
