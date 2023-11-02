124f1d446827121effb5de7d0a09e25ba2976b6e
tamerkat <mahad10taha@gmail.com>
2023-11-02 20:25:34 +0200

    ola

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * array_range - Entry poient
+ * @min: pointer
+ * @max: pointer
+ * Return: always success
+ */
+
+
+int *array_range(int min, int max)
+{
+	int i, len;
+	int *ptr;
+
+	if (min > max)
+		return (NULL);
+	len = max - min + 1;
+	ptr = malloc(sizeof(int) * len);
+	if (ptr == 0)
+		return (NULL);
+	for (; ptr[i] <= max; i++)
+		ptr[i] = min++;
+	return (ptr);
+}
