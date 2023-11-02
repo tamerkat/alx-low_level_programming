6357a56481c18dbee8b9b013a89f7c02e29b8e50
tamerkat <mahad10taha@gmail.com>
2023-11-02 20:31:45 +0200

    ola

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -6,8 +6,11 @@
 #include <stdlib.h>
 #include <limits.h>
 
-char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+char *memest(char *s, char b, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
 
 
 #endif /*MAIN_H*/
