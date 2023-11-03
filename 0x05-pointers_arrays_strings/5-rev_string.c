8d536c26ff15435df00801163501acf672678e53
tamerkat <mahad10taha@gmail.com>
2023-11-03 23:53:17 +0200

    re

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * rev_string - Entry point
+ * @s:pointer
+ * Return: alawys succuss
+*/
+
+void rev_string(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	;
+
+	while (i--)
+	_putchar(s[i]);
+	return (i);
+}
