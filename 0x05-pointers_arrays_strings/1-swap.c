cd40b875c8ec258030ae4c852ba47ff5b5ee7e8c
tamerkat <mahad10taha@gmail.com>
2023-11-03 16:45:13 +0200

    remain

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int -Entry point
+ * @a:pointer
+ * @b:pointer
+ * Return: alawys succus
+*/
+
+void swap_int(int *a, int *b)
+{
+	int *i = a;
+	int *j = b;
+
+	i = b;
+	j = a;
+}
+
