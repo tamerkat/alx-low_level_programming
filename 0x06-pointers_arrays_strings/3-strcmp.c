814acc471841bd2762d0ccb2e3f08915adfd66bc
tamerkat <mahad10taha@gmail.com>
2023-11-05 14:37:15 +0200

    PM

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strcmp -Entry point
+ * @s2: pointer
+ * @s1:pointer
+ * Return: always succuss
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+	int equal = 0;
+
+	while (*s1)
+	if (*s1 != *s2)
+	{
+	equal = ((int)*s1 - 48) - ((int)*s2 - 48);
+	break;
+	}
+	else
+	{
+	*s1++;
+	*s2++;
+	}
+	return (equal);
+}
