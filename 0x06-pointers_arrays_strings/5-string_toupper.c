b74119ecf67f559b59d4b4ecb57f41e6fc1f5559
tamerkat <mahad10taha@gmail.com>
2023-11-05 16:28:47 +0200

    PM

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * string_toupper -Entry point
+ * string_toupper - Entry point
  * @str: pointer
  * Return: always succuss
 */
@@ -12,7 +12,8 @@ char *string_toupper(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-	if (str[i] >= 97 && str[i] <= 122)
-		str[i] = str[i] - 32;
+	if ((int)str[i] >= 97 && (int)str[i] <= 122)
+	str[i] = (int)str[i] - 32;
+	}
 	return (str);
-i}
+}
