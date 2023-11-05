7b433c7c7a4c04d8926767f9d29dfc3f6396072c
tamerkat <mahad10taha@gmail.com>
2023-11-05 14:08:33 +0200

    Mp

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -15,6 +15,9 @@ char *_strncpy(char *dest, char *src, int n)
 	for (i = 0; src[i] != '\0' && i < n; i++)
 		dest[i] = src[i];
 
-	dest[i++] = '\0';
+	while (i < n)
+		dest[i++] = '\0';
+
 	return (dest);
+
 }
