fe5e2d132e0ad7cbf4f305519189dcd046cd8509
tamerkat <mahad10taha@gmail.com>
2023-11-05 14:39:13 +0200

    PM

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,15 +12,15 @@ int _strcmp(char *s1, char *s2)
 	int equal = 0;
 
 	while (*s1)
-	if (*s1 != *s2)
-	{
-	equal = ((int)*s1 - 48) - ((int)*s2 - 48);
-	break;
-	}
-	else
-	{
-	*s1++;
-	*s2++;
-	}
-	return (equal);
+		if (*s1 != *s2)
+		{
+			equal = ((int)*s1 - 48) - ((int)*s2 - 48);
+			break;
+		}
+		else
+		{
+			s1++;
+			s2++;
+		}
+		return (equal);
 }
