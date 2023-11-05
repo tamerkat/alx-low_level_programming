7e5f40718dcec9763fbb2fa494f3e7296f9ec076
tamerkat <mahad10taha@gmail.com>
2023-11-05 16:24:58 +0200

    PM

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -12,7 +12,7 @@ char *string_toupper(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-	if ((int)*str >= 97 || (int)*str <= 122)
-		str[i] = (int)*str - 32;
+	if ((int)str[i] >= 97 || (int)str[i] <= 122)
+		str[i] = (int)str[i] - 32;
 	return (str);
 i}
