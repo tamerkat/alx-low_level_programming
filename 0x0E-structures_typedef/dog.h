752c55e2163887cdd7d71fe7b107e96b41d47ea4
tamerkat <mahad10taha@gmail.com>
2023-11-06 20:32:40 +0200

    pre

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,10 +16,12 @@ struct dog
 	float age;
 	char *owner;
 };
+
 typedef struct dog dog_t;
 
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 void free_dog(dog_t *d);
+
 #endif /* DOG_H */
