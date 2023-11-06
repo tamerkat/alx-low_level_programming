e8af8745f289f1ae528a95c02081570c9b1ae5c5
tamerkat <mahad10taha@gmail.com>
2023-11-06 18:14:40 +0200

    pre

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+this is fucker readme file
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - this is a dog struck
+ * @name:pointer
+ * @age:olso pointer
+ * @owner:also also pointer
+ *
+ * Description: just alone dog
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
