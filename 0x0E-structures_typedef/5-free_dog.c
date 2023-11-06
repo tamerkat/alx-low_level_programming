219b251b796a2b68bc081b3d987a5a42f2a458b5
tamerkat <mahad10taha@gmail.com>
2023-11-06 20:24:56 +0200

    pre

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * free_dog - Entry point
+ * @d: pointer
+*/
+
+
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+	if (d->name)
+	free(d->name);
+	if (d->owner)
+	free(d->owner);
+	free(d);
+	}
+}
