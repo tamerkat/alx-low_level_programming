6dd86098550357cc747f1e9c82d06217122d9686
tamerkat <mahad10taha@gmail.com>
2023-11-06 19:31:29 +0200

    pre

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -3,8 +3,8 @@
 
 /**
  * print_dog - function
- * struct dog - Entry point
  * @d: pointer
+ * Return: void
 */
 
 void print_dog(struct dog *d)
@@ -12,7 +12,7 @@ void print_dog(struct dog *d)
 	if (d)
 	{
 	printf("Name: %s\n", d->name ? d->name : "(nil)");
-	printf("Age: %s\n", d->Age);
+	printf("Age: %s\n", d->age);
 	printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
 	}
 }
