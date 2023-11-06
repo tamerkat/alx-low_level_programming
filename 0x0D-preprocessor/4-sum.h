c2b6ed3c784da05cf0d516d707f5a04abb095938
tamerkat <mahad10taha@gmail.com>
2023-11-06 14:10:54 +0200

    pre

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,6 +1,6 @@
-#ifndef FUKER_BITCH_H
-#define FUKER_BITCH_H
+#ifndef SUM_H
+#define SUM_H
 
-#define SUM(x, y) ((X) + (y))
+#define SUM(x, y) ((x) + (y))
 
-#endif /*FUKER_BITCH_H*/
+#endif /*SUM_H*/
