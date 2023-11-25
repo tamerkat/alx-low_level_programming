27393b8d397a1a9f1680a405615d3d01f910ddf1
tamerkat <mahad10taha@gmail.com>
2023-11-25 22:58:11 +0200

    bitch

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * add_nodeint_end - Entry point for printing
+ * @n: pointer
+ * @head: pointer
+ * Return: pointer to
+*/
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *node = malloc(sizeof(listint_t));
+
+	if (!node || !head)
+	return (NULL);
+
+	node->next = NULL;
+	node->n = n;
+	return (node);
+}
