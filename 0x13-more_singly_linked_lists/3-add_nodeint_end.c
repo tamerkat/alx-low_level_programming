17f73128f0d689e7618b782354d1f96b2d2eca59
tamerkat <mahad10taha@gmail.com>
2023-11-25 23:11:52 +0200

    bitch

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -9,16 +9,23 @@
 
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t *node = malloc(sizeof(listint_t));
+		listint_t *node = malloc(sizeof(listint_t));
+	listint_t *new_node = malloc(sizeof(listint_t));
 
-	if (!node || !head)
+	if (!new_node || !head)
 	return (NULL);
 
 	node->next = NULL;
 	node->n = n;
 
-	if (head)
-		head->next = *node;
-	*node = head
+	if (!*head)
+	*head = new_node;
+	else
+	{
+	node = *head;
+	while (node->next)
+	node = node->next
+	node->next = new_node;
+	}
 	return (node);
 }
