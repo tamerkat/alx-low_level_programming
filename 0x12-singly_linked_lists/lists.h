0cdd18160fd7454a72428b6c87ba940e078c4c8e
tamerkat <mahad10taha@gmail.com>
2023-11-25 15:51:59 +0200

    linked

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -21,7 +21,7 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
-
+list_t *add_node_end(list_t **head, const char *str);
 int _strlen(char *s);
 size_t list_len(const list_t *h);
 size_t print_list(const list_t *h);
