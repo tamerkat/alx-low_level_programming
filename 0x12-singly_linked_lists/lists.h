1e6feceb455d9aa9ab534460d18aa505bed92d93
tamerkat <mahad10taha@gmail.com>
2023-11-25 17:45:03 +0200

    linked

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -27,6 +27,7 @@ size_t list_len(const list_t *h);
 size_t print_list(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 void free_list(list_t *head);
+void _constructor(void) __attribute__ ((constructor));
 
 
 
