fd724a33fb4cabceea7b11375d23c2faeeea41f7
tamerkat <mahad10taha@gmail.com>
2023-11-25 13:16:46 +0200

    linked

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef LISTS_H
+#define LISTS_H
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -16,15 +16,14 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 
-size_t print_list(const list_t *h);
-size_t list_len(const list_t *h);
 int _strlen(char *s);
+size_t list_len(const list_t *h);
+size_t print_list(const list_t *h);
 
-
-#endif /*MAIN_H*/
+#endif /*LISTS*/
