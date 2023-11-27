63e6b0d030d5abfa6a460568f2fd9da9eccec91a
tamerkat <mahad10taha@gmail.com>
2023-11-27 17:15:06 +0200

    bitch

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -15,8 +15,8 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	if (!new_node || !head)
 		return (NULL);
 
-	node->next = NULL;
-	node->n = n;
+	new_node->next = NULL;
+	new_node->n = n;
 
 	if (!*head)
 		*head = new_node;
