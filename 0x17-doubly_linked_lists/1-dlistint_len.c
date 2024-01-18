cc3a947f51cfb9c51392c3b01939b5917b3d86bc
tamerkat <mahad10taha@gmail.com>
2024-01-18 21:57:07 +0200

    py

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - check the code
+ *@h: pointer
+ * Return: Always EXIT_SUCCESS.
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+    size_t i = 0;
+
+    while (h)
+    {
+        h = h->next;
+        i++;
+    }
+    return (i);
+}
--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,9 +1,12 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef __LISTS_H
+#define __LISTS_H
 
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
+
+#define __silent __attribute__((__unused__))
+
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
@@ -21,5 +24,13 @@ typedef struct dlistint_s
 } dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h);
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
+void free_dlistint(dlistint_t *head);
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
+int sum_dlistint(dlistint_t *head);
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 
 #endif
