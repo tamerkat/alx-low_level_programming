af3665d705c418d9c0f18fa4a7a4e02138033d10
tamerkat <mahad10taha@gmail.com>
2024-02-02 17:25:22 +0200

    py

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,15 +13,17 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	new_table = malloc(sizeof(hash_table_t));
 	if (new_table == NULL)
-	return (NULL);
+		return (NULL);
 
 	new_table->size = size;
 	new_table->array = malloc(size * sizeof(hash_node_t *));
 	if (new_table->array == NULL)
-	free(new_table);
-	return (NULL);
+	{
+		free(new_table);
+		return (NULL);
+	}
 
 	for (i = 0; i < size; i++)
-	new_table->array[i] = NULL;
+		new_table->array[i] = NULL;
 	return (new_table);
 }
