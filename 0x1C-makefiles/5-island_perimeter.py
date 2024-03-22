1f53ed54e8c8aef3f85aa0c30e24870b9094300e
tamerkat <mahad10taha@gmail.com>
2024-03-22 16:46:36 +0200

    update

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def island_perimeter(grid):
+    p = 0
+    for row in range(len(grid)):
+        for col in range(len(grid[0])):
+            if grid[row][col] == 1:
+                if row == 0 or grid[row - 1][col] == 0:
+                    p += 1
+                if row == (len(grid) - 1) or grid[row + 1][col] == 0:
+                    p += 1
+                if col == 0 or grid[row][col - 1] == 0:
+                    p += 1
+                if col == (len(grid[0]) - 1) or grid[row][col + 1] == 0:
+                    p += 1
+    return p
