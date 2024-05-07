18dec5f499832b1b0ae49e73a97be46eb711b1d1
tamerkat <mahad10taha@gmail.com>
2024-05-07 14:12:21 +0300

    liner algo

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -6,19 +6,20 @@
  * linear_search - Entry point
  * @array: list
  * @value: integer
+ * @size: int
  * Return: Always EXIT_SUCCESS
  */
 
 int linear_search(int *array, size_t size, int value)
 {
-    if (!array)
-        return (-1);
+	if (!array)
+		return (-1);
 
-    for (int i = 0; i < size; i++)
-    {
-        if (array[i] == value)
-            return (i - 1);
-        else
-            return (-1);
-    }
+	for (int i = 0; i < size; i++)
+	{
+		if (array[i] == value)
+			return (i - 1);
+		else
+			return (-1);
+	}
 }
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -8,4 +8,4 @@
 
 int linear_search(int *array, size_t size, int value);
 
-#endif 
+#endif
