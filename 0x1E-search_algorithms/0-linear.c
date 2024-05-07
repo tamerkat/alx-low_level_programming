49d1f1dedf7a5072cfca1b016acdd1b2fc41c47a
tamerkat <mahad10taha@gmail.com>
2024-05-07 15:00:47 +0300

    liner algo

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -5,21 +5,21 @@
 /**
  * linear_search - Entry point
  * @array: list
+ * @size: size_t
  * @value: integer
- * @size: int
  * Return: Always EXIT_SUCCESS
  */
 
 int linear_search(int *array, size_t size, int value)
 {
-	size_t i;
+	int i;
 
-	if (!array)
+	if (array == NULL)
 		return (-1);
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < (int)size; i++)
 	{
-		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		printf("Value checked array[%u] = [%d]\n", i, array[i]);
 		if (value == array[i])
 			return (i);
 	}
