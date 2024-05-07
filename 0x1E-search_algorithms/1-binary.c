9940e6f41649c0df89e762342504a96c1fd87548
tamerkat <mahad10taha@gmail.com>
2024-05-07 16:16:20 +0300

    liner algo

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "search_algos.h"
+
+/**
+ * binary_search - Entry point
+ *@array: list
+ *@size: size_t
+ *@value: int
+ * Return: Always EXIT_SUCCESS
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+	int low = 0;
+	int high = size - 1;
+
+	while (low < high)
+	{
+	int mid = (low + high) / 2;
+
+	if (value == array[mid])
+		return (mid);
+	else if (array[mid] < value)
+		low = mid + 1;
+	else
+		high = mid - 1;
+	}
+	return (-1);
+}
