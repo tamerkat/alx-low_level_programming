7b58f27975f8a6c18d34a9f10caa9add4481a33b
tamerkat <mahad10taha@gmail.com>
2024-05-07 14:10:03 +0300

    liner algo

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "search_algos.h"
+
+/**
+ * linear_search - Entry point
+ * @array: list
+ * @value: integer
+ * Return: Always EXIT_SUCCESS
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+    if (!array)
+        return (-1);
+
+    for (int i = 0; i < size; i++)
+    {
+        if (array[i] == value)
+            return (i - 1);
+        else
+            return (-1);
+    }
+}
