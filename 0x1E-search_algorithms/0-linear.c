b39367f339707e8ca99ab15bfbc1f37b869500fd
tamerkat <mahad10taha@gmail.com>
2024-05-07 14:45:43 +0300

    liner algo

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -15,14 +15,15 @@ int linear_search(int *array, size_t size, int value)
 	size_t i;
 
 	if (!array)
-	return (-1);
+		return (-1);
 
 	for (i = 0; i < size; i++)
 	{
-	if (array[i] == value)
-	{
-	return (i);
-	}
+		if (array[i] == value)
+		{
+			printf("Value checked array[%d] = [%d]\n", i, array[i]);
+			return (i);
+		}
 	}
 	return (-1);
 }
