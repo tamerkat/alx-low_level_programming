cf6623b0cca0cc8ec4145ed841426e51f7f124bb
tamerkat <mahad10taha@gmail.com>
2024-05-07 16:26:18 +0300

    liner algo

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -17,14 +17,15 @@ int binary_search(int *array, size_t size, int value)
 
 	while (low < high)
 	{
-	int mid = (low + high) / 2;
+		printf("Searching in array: ");
+		int mid = (low + high) / 2;
 
-	if (value == array[mid])
-		return (mid);
-	else if (array[mid] < value)
-		low = mid + 1;
-	else
-		high = mid - 1;
+		if (value == array[mid])
+			return (mid);
+		else if (array[mid] < value)
+			low = mid + 1;
+		else
+			high = mid - 1;
 	}
 	return (-1);
 }
